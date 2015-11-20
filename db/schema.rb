6226ba3216b3293d8bca1deaeafeb6f7ad5317a3
Matt Brendzel <brendzel@gmail.com>
2015-11-20 16:26:58 -0500

    Creates and executes migration associating each 'translation' with a 'snippet'.
    One-to-many relationship: one 'snippet' is associated with many 'translations'.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151120212341) do
+ActiveRecord::Schema.define(version: 20151120212537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,9 +72,13 @@ ActiveRecord::Schema.define(version: 20151120212341) do
     t.text     "content",    null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "snippet_id"
   end
+
+  add_index "translations", ["snippet_id"], name: "index_translations_on_snippet_id", using: :btree
 
   add_foreign_key "documents", "profiles"
   add_foreign_key "profiles", "accounts"
   add_foreign_key "snippets", "documents"
+  add_foreign_key "translations", "snippets"
 end
