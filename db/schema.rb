f6840ade7663d5cf09ae1b35cc118361685f96a0
Bernard Lee <bernardlee82@gmail.com>
2016-02-17 10:32:12 -0500

    Ensure that password digests are non-null
    Add a null: false property to the password_digest column.

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20151120234230) do
   create_table "accounts", force: :cascade do |t|
     t.string   "email",           null: false
     t.string   "token",           null: false
-    t.string   "password_digest"
+    t.string   "password_digest", null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
